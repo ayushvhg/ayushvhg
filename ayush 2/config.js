ef597c7b6ea3a0f3d64a3be19d42e04607b8c1c5
ayushvhg <ayushvhg@gmail.com>
2025-01-20 08:21:37 +0530

    new commit

---
+++ ayush 2/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBwkJ48MPgvrRJtaIhWdTGB-oHnZ4rIzGU",
+    authDomain: "ayush-virtual-herbal-garden.firebaseapp.com",
+    projectId: "ayush-virtual-herbal-garden",
+    storageBucket: "ayush-virtual-herbal-garden.firebasestorage.app",
+    messagingSenderId: "742435630632",
+    appId: "1:742435630632:web:5599357c09e95f70a5cf1d",
+    measurementId: "G-VTD40R0ESQ"
+  };
+  firebase.initializeApp(firebaseConfig);
